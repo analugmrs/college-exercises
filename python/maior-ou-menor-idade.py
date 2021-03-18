338d4faaa90cd556613d5eb82d8415c42a5e3e69
analugmrs <65254047+analugmrs@users.noreply.github.com>
2021-03-17 21:02:44 -0300

    Update and rename main.py to python/maior-ou-menor-idade.py

--- python/maior-ou-menor-idade.py
+++ python/maior-ou-menor-idade.py
@@ -7,6 +7,7 @@ Write your code in this editor and press "Run" button to execute it.
 '''
 
 # Faça um programa que receba a idade do usuário e diga se ele é maior ou menor de idade.
+
 print("Olá, seja bem-vindo!")
 idade = int(input("E aí, qual sua idade? "))
 if idade >= 18:
