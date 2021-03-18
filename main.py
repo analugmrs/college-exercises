e66f40d5569926eecda121429724a395e104ddc3
analugmrs <65254047+analugmrs@users.noreply.github.com>
2021-03-17 21:00:46 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+# Faça um programa que receba a idade do usuário e diga se ele é maior ou menor de idade.
+print("Olá, seja bem-vindo!")
+idade = int(input("E aí, qual sua idade? "))
+if idade >= 18:
+    print("Parabéns, você já é maior de idade!")
+else:
+    print("Ops, você não tem idade suficiente para estar aqui, xiii... =X")
+
